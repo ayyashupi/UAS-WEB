b6b6c587da07e4d9ea48444e5104d0c574cdf3bd
TheBear <95322643+Zhofron@users.noreply.github.com>
2022-03-29 19:39:56 +0700

    Anmasi Home

---
+++ home.js
@@ -0,0 +1,46 @@
+var id = null;
+
+function Test() {
+	alert("Hi Hi");
+}
+
+function AnimasiWelcome() {
+	var pos = 1;
+	var div = document.getElementById("welcome");
+	clearInterval(id);
+	id = setInterval(hover,4);
+	//id = setInterval(fadein, 10);
+	var i = 0;
+	function fadein() {
+		if (pos == 450) {
+    		clearInterval(id);
+    		div.style.opacity = 1;
+    		
+   		} else {
+  			pos++;
+    		div.style.marginTop = (pos/10) - 50 +'%';
+  	  		div.style.opacity = (pos * (1/500));
+ 	   	}
+	}
+
+	pos = 0;
+    var increase = true;
+	function hover(){
+    	if (pos == 1000) {
+    		increase = false;
+    		pos--;
+    	}else if(pos == -200) {
+    		increase = true;
+    		pos++;
+    	}else{
+    		if(increase == true){
+    			pos++;
+    		}else{
+    			pos--;
+    		}
+    		div.style.marginLeft = (pos/5) + "px";
+    		div.style.marginBottom = (pos/5) + "px";
+    	}
+    }
+}
+
