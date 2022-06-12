e26edcfbf8be6a9527c31e2b83741a5a8501dc2e
ayyashupi <ayyash@upi.edu>
2022-06-12 22:49:51 +0700

    Like into session and database
    YOLO

--- galeriTest.php
+++ galeriTest.php
@@ -5,6 +5,7 @@
 session_start();
 
 if(isset($_SESSION['user_name'])||isset($_SESSION['admin_name'])){
+<<<<<<< Updated upstream
 	if(!empty($_POST['search'])){
 		$search = $_POST['search'];
 		$datagambar = null;
@@ -20,6 +21,16 @@ if(isset($_SESSION['user_name'])||isset($_SESSION['admin_name'])){
       	$datagambar = null;
       	$select = " SELECT * FROM gambar";
       	$result = mysqli_query($conn, $select);
+=======
+   
+}else{
+   header('location:login_form.php');
+}
+
+$datagambar = null;
+$select = " SELECT * FROM gambar";
+$result = mysqli_query($conn, $select);
+>>>>>>> Stashed changes
 // $jumlah = 1;
       	$jumlah = 0;
 while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
@@ -33,10 +44,33 @@ while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through re
 	header('location:login_form.php');
 }
 
+// mysqli_select_db($conn, $loekis);
+if(isset($_POST['liked'])){
+  $post_id = $_POST['post_id'];
+  $result = mysqli_query($conn, "SELECT * FROM gambar WHERE id = $post_id");
+  $row = mysqli_fetch_array($result);
+  $n = $row['jumlah_like'];
+
+  mysqli_query($conn, "UPDATE gambar SET jumlah_like = $n+1 WHERE  id = $post_id");
+  mysqli_query($conn, "INSERT INTO user_likes (user_id, post_id) VALUES (".$_SESSION['user_name'].", $post_id)");
+  exit();
+}
+if(isset($_POST['unliked'])){
+   $post_id = $_POST['post_id'];
+   $result = mysqli_query($conn, "SELECT * FROM gambar WHERE id = $post_id");
+   $row = mysqli_fetch_array($result);
+   $n = $row['jumlah_like'];
+ 
+   mysqli_query($conn, "DELETE FROM user_likes WHERE post_id = $post_id AND user_id =".$_SESSION['id']);
+   mysqli_query($conn, "UPDATE gambar SET jumlah_like = $n-1 WHERE  id = $post_id");
+   exit();
+ }
+
 
 
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -54,6 +88,7 @@ while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through re
 </head>
 <body class="body">
 
+<<<<<<< Updated upstream
 	<div class="main-container d-flex">
 		<div class="sidebar" id="side_nav">
 			<div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
@@ -117,6 +152,56 @@ while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through re
 
 				<!-- Galeri -->
 				<div class="dashboard-content px-3 pt-4">
+=======
+<div class="main-container d-flex">
+      <div class="sidebar" id="side_nav">
+         <div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
+            <h1 class="fs-4"><span class="text-white px-3 py-2">Menoe</span></h1>
+            <button class="btn close-btn px-1 py-0 text-white">
+               <i class="fa-solid fa-bars fa-xl "></i></button>
+         </div>
+         <ul class="list-unstyled px-2">
+            <li class="active"><a href="galeri.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-images"></i> Baki Tjitra</a></li>
+            <li class=""><a href="kirimpesan.php" class="text-decoration-none px-3 py-2 d-block">
+               <span><i class="fa-solid fa-envelope"></i> Kotak Soerat</span>
+               <!-- <span class="bg-dark rounded-pill text-white py-0 px-2">01</span> -->
+            </a>
+            </li>
+            <li class=""><a href="musik.php" class="text-decoration-none px-3 py-2 d-block d-flex justify-content-between">
+            <span><i class="fa-solid fa-compact-disc"></i> Irama Nusantara</span></a></li>
+         </ul>
+         <hr class="h-color mx-2">
+         <ul class="list-unstyled px-2">
+            <li class=""><a href="akun.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-user"></i> Akoen</a></li>
+            <li class=""><a href="logout.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-arrow-right-from-bracket"></i> Keloear</a></li>
+         </ul>
+      </div>
+      <div class="content">
+         <nav class="navbar navbar-expand-md navbar-dark">
+            <div class="container-fluid">
+            <div class="d-flex justify-content-between">
+            <a class="navbar-brand fs-4" href="#"></a>
+            <button class="btn px-1 py-0 open-btn text-white">
+               <i class="fa-solid fa-bars fa-lg"></i></button>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button> 
+            <div class="collapse navbar-collapse" id="toggleMobileMenu">
+               <ul class="navbar-nav ms-auto">
+                  <li class="nav-item">
+                     <a class="nav-link active pe-3" aria-current="page" href="home.php">L</a>
+                  </li>
+               </ul>
+            </div>
+            </div>
+         </nav>
+
+         <!-- Galeri -->
+         <div class="dashboard-content px-3 pt-4">
+>>>>>>> Stashed changes
             <!-- <h2 class="fs-5 px-3"><img class="mx-auto d-block" src="/images/logo.png" width="75%" height="75%"></h2>
             	<p class="px-3"></p> -->
         <!-- <div class="container-fluid">
@@ -136,13 +221,32 @@ while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through re
    </div> -->
    <!-- End -->
 
+<<<<<<< Updated upstream
    <div class="row row-cols-3 px-3 pb-3">
    	<!-- Gallery item -->
    	<!-- End -->
+=======
+    <div class="row row-cols-3 px-3 pb-3">
+      <!-- Gallery item -->
+      <!-- <div class="col">
+        <div class="bg-black rounded shadow-sm"><img src="https://bootstrapious.com/i/snippets/sn-gallery/img-1.jpg" alt="" class="img-fluid card-img-top">
+          <div class="p-4">
+            <h5> <a href="#" class="text-light">Red paint cup</a></h5>
+            <p class="small text-muted-light mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+            <div class="d-flex align-items-center justify-content-between px-3 py-2 mt-4">
+              <p class="small mb-0"><i class="fa-solid fa-heart"></i></p>
+              <div class="badge badge-danger px-3 rounded-pill font-weight-normal"><i class="fa-solid fa-flag"></i></div>
+            </div>
+          </div>
+        </div>
+      </div> -->
+      <!-- End -->
+>>>>>>> Stashed changes
 
    	<?php 
    	for ($i=0; $i < $jumlah; $i++) { 
         # code...
+<<<<<<< Updated upstream
    		echo "<div class='col'>";
    		echo "<div class='bg-black rounded shadow-sm'><img src='".$datagambar[$i]["link_gambar"]."' alt='' class='img-fluid card-img-top'>";
    		echo "<div class='p-4'>";
@@ -158,6 +262,40 @@ while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through re
    	}
    	?>
 
+=======
+        echo "<div class='col'>";
+        echo "<div class='bg-black rounded shadow-sm'><img src='".$datagambar[$i]["link_gambar"]."' alt='' class='img-fluid card-img-top'>";
+        echo "<div class='p-4'>";
+        echo "<h5> <a href='' class='text-light'>".$datagambar[$i]["judul"]."</a></h5>";
+        echo "<p class='small text-muted-light mb-0'>".$datagambar[$i]["pelukis"]." - ".$datagambar[$i]["tahun"]."</p>";
+        echo "<div class='d-flex align-items-center justify-content-between px-3 py-2 mt-4'>";
+      
+      $result  = mysqli_query($conn, "SELECT * FROM user_likes where user_id=".$_SESSION['id']." and post_id=".$row['id']."");
+      if (mysqli_num_rows($result) == $row['id']) {
+     
+         echo  "<span><a href='' class='unlike' id='".$row['id']."'><i class='fa-regular fa-heart'></i></a></span>";
+      } else {
+         echo  "<span><a href='' class='like' id='".$row['id']."'><i class='fa-solid fa-heart'></i></a></span>";
+         
+      } 
+      //   echo "<p class='small mb-0'><a href='' id='btnlr'><i class='fa-solid fa-heart'></i></a></p>";
+        echo "<div class='badge badge-danger px-3 rounded-pill font-weight-normal'><a href='kirimpesan.php?id=".$datagambar[$i]["id"]."' class='btn btn-link text-decoration-none text-white' id='btnlr'><i class='fa-solid fa-flag'></i></a></div>";
+        echo "</div>";
+        echo "</div>"; 
+        echo "</div>";
+        echo "</div>";
+      }
+      ?>
+
+      
+
+    </div>
+    <!-- <div class="py-5 text-right"><a href="#" class="btn btn-dark px-5 py-3 text-uppercase">Show me more</a></div> -->
+  </div>
+</div>
+         </div>
+      </div>
+>>>>>>> Stashed changes
    </div>
    <!-- <div class="py-5 text-right"><a href="#" class="btn btn-dark px-5 py-3 text-uppercase">Show me more</a></div> -->
 </div>
@@ -191,6 +329,46 @@ crossorigin="anonymous"></script>
 		$('.close-btn').hide();
 		$('.open-btn').show();
 	});
+
+$(document).ready(function(){
+   $('.like').click(function(){
+      var post_id = $(this).attr('id');
+      $.ajax({
+         url:'galeri.php',
+         type:'POST',
+         async: false,
+         data:{
+            'liked':1,
+            'post_id':post_id
+
+         },
+         success:function(){
+            // $('#'+post_id+'.like').hide();
+            // $('#'+post_id+'.unlike').show();
+         }
+         });
+      });
+
+      $('.unlike').click(function(){
+      var post_id = $(this).attr('id');
+      $.ajax({
+         url:'galeri.php',
+         type:'POST',
+         async: false,
+         data:{
+            'unliked':1,
+            'post_id':post_id
+
+         },
+         success:function(){
+            // $('#'+post_id+'.like').hide();
+            // $('#'+post_id+'.unlike').show();
+         }
+         });
+      });
+
+   });   
+
 
 </script>
 </body>
