c421e1757d8fe2574c857ab9d9b8c78e79111fa8
TheBear <95322643+Zhofron@users.noreply.github.com>
2022-06-12 21:30:05 +0700

    Galeri Sort By

--- galeri.php
+++ galeri.php
@@ -5,30 +5,43 @@
 session_start();
 
 if(isset($_SESSION['user_name'])||isset($_SESSION['admin_name'])){
-   if(!empty($_POST['search'])){
-    $search = $_POST['search'];
-    $datagambar = null;
-    $select = " select * from gambar where id like '%$search%' or judul like '%$search%'";
-    $result = mysqli_query($conn, $select);
+	$sorter = "id";
+	$order = " desc";
+	if(!empty($_POST['search'])){
+		$search = $_POST['search'];
+		$sorter = $_POST['sorter'];
+		if ($sorter == "judul") {
+			$order = " asc";
+		}
+		$datagambar = null;
+		$select = " select * from gambar where id like '%$search%' or judul like '%$search%' order by ".$sorter.$order;
+		$result = mysqli_query($conn, $select);
         // $jumlah = 1;
-    $jumlah = 0;
+		$jumlah = 0;
          while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-          $datagambar[$jumlah] = $row;
-          $jumlah++;
+         	$datagambar[$jumlah] = $row;
+         	$jumlah++;
          }
       }else{
-        $datagambar = null;
-        $select = " SELECT * FROM gambar";
-        $result = mysqli_query($conn, $select);
+      	if (!empty($_POST['sorter'])) {
+      		$sorter = $_POST["sorter"];
+      		if ($sorter == "judul") {
+      			$order = " asc";
+      		}
+      	}
+
+      	$datagambar = null;
+      	$select = " SELECT * FROM gambar order by ".$sorter.$order;
+      	$result = mysqli_query($conn, $select);
 // $jumlah = 1;
-        $jumlah = 0;
+      	$jumlah = 0;
       while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-        $datagambar[$jumlah] = $row;
-        $jumlah++;
+      	$datagambar[$jumlah] = $row;
+      	$jumlah++;
       }
-    }
+   }
 }else{
-   header('location:login_form.php');
+	header('location:login_form.php');
 }
 
 ?>
@@ -37,85 +50,96 @@ if(isset($_SESSION['user_name'])||isset($_SESSION['admin_name'])){
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="css/galeri.css">
-<link rel="stylesheet" href="css/home.css">
-<!-- <link rel="stylesheet" href="/css/menubar.css"> -->
-<link rel="stylesheet" href="css/fonts.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-crossorigin="anonymous">
-<script src="https://kit.fontawesome.com/5e6fe494a2.js" crossorigin="anonymous"></script>
-<link rel="icon" href="images/icon.png" type="image/png">
-<title>Baki Tjitra (Galeri)</title>
+	<link rel="stylesheet" href="css/galeri.css">
+	<link rel="stylesheet" href="css/home.css">
+	<!-- <link rel="stylesheet" href="/css/menubar.css"> -->
+	<link rel="stylesheet" href="css/fonts.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+	rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+	crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/5e6fe494a2.js" crossorigin="anonymous"></script>
+	<link rel="icon" href="images/icon.png" type="image/png">
+	<title>Baki Tjitra (Galeri)</title>
 </head>
 <body class="body">
 
-<div class="main-container d-flex">
-      <div class="sidebar" id="side_nav">
-         <div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
-            <h1 class="fs-4"><span class="text-white px-3 py-2">Menoe</span></h1>
-            <button class="btn close-btn px-1 py-0 text-white">
-               <i class="fa-solid fa-bars fa-xl "></i></button>
-         </div>
-         <ul class="list-unstyled px-2">
-            <li class="active"><a href="galeri.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-images"></i> Baki Tjitra</a></li>
-            <li class=""><a href="kirimpesan.php" class="text-decoration-none px-3 py-2 d-block">
-               <span><i class="fa-solid fa-envelope"></i> Kotak Soerat</span>
-               <!-- <span class="bg-dark rounded-pill text-white py-0 px-2">01</span> -->
-            </a>
-            </li>
-            <li class=""><a href="musik.php" class="text-decoration-none px-3 py-2 d-block d-flex justify-content-between">
-            <span><i class="fa-solid fa-compact-disc"></i> Irama Nusantara</span></a></li>
-         </ul>
-         <hr class="h-color mx-2">
-         <ul class="list-unstyled px-2">
-            <li class=""><a href="akun.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-user"></i> Akoen</a></li>
-            <li class=""><a href="logout.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-arrow-right-from-bracket"></i> Keloear</a></li>
-         </ul>
-      </div>
-      <div class="content">
-         <nav class="navbar navbar-expand-md navbar-dark">
-            <div class="container-fluid">
-            <div class="d-flex justify-content-between">
-            <a class="navbar-brand fs-4" href="#"></a>
-            <button class="btn px-1 py-0 open-btn text-white">
-               <i class="fa-solid fa-bars fa-lg"></i></button>
-            </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button> 
-            <form method="post" action="#" style="width: 50%;">
-              <div class="row g-3 ms-5" >
+	<div class="main-container d-flex">
+		<div class="sidebar" id="side_nav">
+			<div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
+				<h1 class="fs-4"><span class="text-white px-3 py-2">Menoe</span></h1>
+				<button class="btn close-btn px-1 py-0 text-white">
+					<i class="fa-solid fa-bars fa-xl "></i></button>
+				</div>
+				<ul class="list-unstyled px-2">
+					<li class="active"><a href="galeri.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-images"></i> Baki Tjitra</a></li>
+					<li class=""><a href="kirimpesan.php" class="text-decoration-none px-3 py-2 d-block">
+						<span><i class="fa-solid fa-envelope"></i> Kotak Soerat</span>
+						<!-- <span class="bg-dark rounded-pill text-white py-0 px-2">01</span> -->
+					</a>
+				</li>
+				<li class=""><a href="musik.php" class="text-decoration-none px-3 py-2 d-block d-flex justify-content-between">
+					<span><i class="fa-solid fa-compact-disc"></i> Irama Nusantara</span></a></li>
+				</ul>
+				<hr class="h-color mx-2">
+				<ul class="list-unstyled px-2">
+					<li class=""><a href="akun.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-user"></i> Akoen</a></li>
+					<li class=""><a href="logout.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-arrow-right-from-bracket"></i> Keloear</a></li>
+				</ul>
+			</div>
+			<div class="content">
+				<nav class="navbar navbar-expand-md navbar-dark">
+					<div class="container-fluid">
+						<div class="d-flex justify-content-between">
+							<a class="navbar-brand fs-4" href="#"></a>
+							<button class="btn px-1 py-0 open-btn text-white">
+								<i class="fa-solid fa-bars fa-lg"></i></button>
+							</div>
+							<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+							data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu"
+							aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button> 
+						<form method="post" action="#" style="width: 50%;">
+							<div class="row g-3 ms-5" >
 
-                <div class="col">
-                  <input type="text" class="form-control" placeholder="Nama Gambar" aria-label="First name" name="search">
-                </div>
-                <div class="col">
-                  <button type="submit" class="btn btn-outline-success my-2 my-sm-0" placeholder="Last name" aria-label="Last name" name="save">Submit</button>
-                </div>
+								<div class="col">
+									<input type="text" class="form-control" placeholder="Nama Gambar" aria-label="First name" name="search">
+								</div>
+								<div class="col">
+									<select class="form-select" aria-label="Default select example" name="sorter">
+										<option selected value="id">Sort By</option>
+										<option value="judul">Judul</option>
+										<option value="jumlah_like">Like</option>
+										<option value="tahun">Tahun</option>
+									</select>
+									
+								</div>
+								<div class="col">
+									<button type="submit" class="btn btn-outline-success my-2 my-sm-0" style="width: 50%;" placeholder="Last name" aria-label="Last name" name="save">Submit</button>
+								</div>
+								
+								
 
-              </div>
-            </form>
-            <div class="collapse navbar-collapse" id="toggleMobileMenu">
-               <ul class="navbar-nav ms-auto">
-                  <li class="nav-item">
-                     <a class="nav-link active pe-3" aria-current="page" href="home.php">L</a>
-                  </li>
-               </ul>
-            </div>
-            </div>
-         </nav>
+							</div>
+						</form>
+						<div class="collapse navbar-collapse" id="toggleMobileMenu">
+							<ul class="navbar-nav ms-auto">
+								<li class="nav-item">
+									<a class="nav-link active pe-3" aria-current="page" href="home.php">L</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
 
-         <!-- Galeri -->
-         <div class="dashboard-content px-3 pt-4">
+				<!-- Galeri -->
+				<div class="dashboard-content px-3 pt-4">
             <!-- <h2 class="fs-5 px-3"><img class="mx-auto d-block" src="/images/logo.png" width="75%" height="75%"></h2>
-            <p class="px-3"></p> -->
+            	<p class="px-3"></p> -->
         <!-- <div class="container-fluid">
-  <div class="px-lg-5"> -->
+        	<div class="px-lg-5"> -->
 
-    <!-- For demo purpose -->
+        		<!-- For demo purpose -->
     <!-- <div class="row py-5">
       <div class="col-lg-12 mx-auto">
         <div class="text-white p-5 shadow-sm rounded banner">
@@ -126,11 +150,11 @@ crossorigin="anonymous">
           </p>
         </div>
       </div>
-    </div> -->
-    <!-- End -->
+   </div> -->
+   <!-- End -->
 
-    <div class="row row-cols-3 px-3 pb-3">
-      <!-- Gallery item -->
+   <div class="row row-cols-3 px-3 pb-3">
+   	<!-- Gallery item -->
       <!-- <div class="col">
         <div class="bg-black rounded shadow-sm"><img src="https://bootstrapious.com/i/snippets/sn-gallery/img-1.jpg" alt="" class="img-fluid card-img-top">
           <div class="p-4">
@@ -142,36 +166,36 @@ crossorigin="anonymous">
             </div>
           </div>
         </div>
-      </div> -->
-      <!-- End -->
+     </div> -->
+     <!-- End -->
 
-      <?php 
-      for ($i=0; $i < $jumlah; $i++) { 
+     <?php 
+     for ($i=0; $i < $jumlah; $i++) { 
         # code...
-        echo "<div class='col'>";
-        echo "<div class='bg-black rounded shadow-sm'><img src='".$datagambar[$i]["link_gambar"]."' alt='' class='img-fluid card-img-top'>";
-        echo "<div class='p-4'>";
-        echo "<h5> <a href='' class='text-light'>".$datagambar[$i]["judul"]."</a></h5>";
-        echo "<p class='small text-muted-light mb-0'>".$datagambar[$i]["pelukis"]." - ".$datagambar[$i]["tahun"]."</p>";
-        echo "<div class='d-flex align-items-center justify-content-between px-3 py-2 mt-4'>";
-        echo "<p class='small mb-0'><button class='btn btn-link text-decoration-none text-white' id='btnlr'><i class='fa-solid fa-heart'></i></button></p>";
-        echo "<div class='badge badge-danger px-3 rounded-pill font-weight-normal'><a href='kirimpesan.php?id=".$datagambar[$i]["id"]."' class='btn btn-link text-decoration-none text-white' id='btnlr'><i class='fa-solid fa-flag'></i></a></div>";
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-      }
-       ?>
+     	echo "<div class='col'>";
+     	echo "<div class='bg-black rounded shadow-sm'><img src='".$datagambar[$i]["link_gambar"]."' alt='' class='img-fluid card-img-top'>";
+     	echo "<div class='p-4'>";
+     	echo "<h5> <a href='' class='text-light'>".$datagambar[$i]["judul"]."</a></h5>";
+     	echo "<p class='small text-muted-light mb-0'>".$datagambar[$i]["pelukis"]." - ".$datagambar[$i]["tahun"]."</p>";
+     	echo "<div class='d-flex align-items-center justify-content-between px-3 py-2 mt-4'>";
+     	echo "<p class='small mb-0'><button class='btn btn-link text-decoration-none text-white' id='btnlr'><i class='fa-solid fa-heart'></i></button></p>";
+     	echo "<div class='badge badge-danger px-3 rounded-pill font-weight-normal'><a href='kirimpesan.php?id=".$datagambar[$i]["id"]."' class='btn btn-link text-decoration-none text-white' id='btnlr'><i class='fa-solid fa-flag'></i></a></div>";
+     	echo "</div>";
+     	echo "</div>";
+     	echo "</div>";
+     	echo "</div>";
+     }
+     ?>
 
-    </div>
-    <!-- <div class="py-5 text-right"><a href="#" class="btn btn-dark px-5 py-3 text-uppercase">Show me more</a></div> -->
   </div>
+  <!-- <div class="py-5 text-right"><a href="#" class="btn btn-dark px-5 py-3 text-uppercase">Show me more</a></div> -->
 </div>
-         </div>
-      </div>
-   </div>
+</div>
+</div>
+</div>
+</div>
 
-   
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
@@ -181,21 +205,21 @@ integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
 crossorigin="anonymous"></script>
 
 <script>
-$('.sidebar ul li').on('click', function(){
-    $('.sidebar ul li.active').removeClass('active');
-    $(this).addClass('active');
-});
+	$('.sidebar ul li').on('click', function(){
+		$('.sidebar ul li.active').removeClass('active');
+		$(this).addClass('active');
+	});
 
-$('.open-btn').on('click', function(){
-   $('.sidebar').addClass('active');
-   $('.open-btn').hide();
-   $('.close-btn').show();
-});
-$('.close-btn').on('click', function(){
-   $('.sidebar').removeClass('active');
-   $('.close-btn').hide();
-   $('.open-btn').show();
-});
+	$('.open-btn').on('click', function(){
+		$('.sidebar').addClass('active');
+		$('.open-btn').hide();
+		$('.close-btn').show();
+	});
+	$('.close-btn').on('click', function(){
+		$('.sidebar').removeClass('active');
+		$('.close-btn').hide();
+		$('.open-btn').show();
+	});
 
 </script>
 </body>
